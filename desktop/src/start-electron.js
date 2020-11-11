80551c6ceba8b962a685f61a284c97adf625a981
stevanmesser <34461083+stevanmesser@users.noreply.github.com>
2020-11-11 20:26:51 -0300

    Creating

--- desktop/src/start-electron.js
+++ desktop/src/start-electron.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 
+const path = require('path');
+const isDev = require('electron-is-dev');
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1440,
@@ -9,7 +12,11 @@ function createWindow() {
     },
   });
 
-  win.loadURL('http://localhost:3000');
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 
   // win.loadFile('index.html');
   win.webContents.openDevTools();
