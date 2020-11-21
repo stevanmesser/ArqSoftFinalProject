82903f623a0e73707072981c93456b10d68ae6ae
Joao Miguel Stroher <joao.stroher@gmail.com>
2020-11-21 15:44:40 -0300

    create mail server

---
+++ mailServer/src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import Mail from './lib/Mail';
+
+const routes = new Router();
+
+routes.post('/sendMail', async (req, res) => {
+  const { to, subject, message } = req.body;
+  await Mail.sendMail({
+    to,
+    subject,
+    template: 'default',
+    context: {
+      message,
+    },
+  });
+  return res.status(200).end();
+});
+
+export default routes;
